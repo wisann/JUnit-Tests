c49725f9b0c3d85507a5b243ef3594d5de70507c
wisan <wisansilva3@gmail.com>
2024-03-08 16:45:38 -0300

    practicing JUnit tests

--- src/test/java/br/com/wisan/math/SimpleMathTest.java
+++ src/test/java/br/com/wisan/math/SimpleMathTest.java
@@ -1,5 +1,7 @@
 package br.com.wisan.math;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,11 +10,75 @@ import br.com.wisan.SimpleMath;
 class SimpleMathTest {
 
 	@Test
-	void test() {
+	void testSum() {
 		SimpleMath math = new SimpleMath();
-		Double actual = math.sum(6.2D, 2D);
+		Double firstNumber = 6.2D;
+		Double secondNumber = 2D;
+		Double actual = math.sum(firstNumber, secondNumber);
 		Double expected = 8.2D;
-		Assertions.assertEquals(expected, actual);
+		Assertions.assertEquals(expected, actual,
+				() -> firstNumber + " + " + secondNumber + " Did not produce " + expected);
+		assertNotEquals(9.2, actual);
+		assertNotNull(actual);
+
+	}
+
+	@Test
+	void testSubstraction() {
+		SimpleMath math = new SimpleMath();
+		Double firstNumber = 6.2D;
+		Double secondNumber = 2D;
+		Double actual = math.subtraction(firstNumber, secondNumber);
+		Double expected = 4.2D;
+		Assertions.assertEquals(expected, actual,
+				() -> firstNumber + " - " + secondNumber + " Did not produce " + expected);
+
+	}
+
+	@Test
+	void testMultiplication() {
+		SimpleMath math = new SimpleMath();
+		Double firstNumber = 2D;
+		Double secondNumber = 2D;
+		Double actual = math.multiplication(firstNumber, secondNumber);
+		Double expected = 4D;
+		Assertions.assertEquals(expected, actual,
+				() -> firstNumber + " * " + secondNumber + " Did not produce " + expected);
+
+	}
+
+	@Test
+	void TestDivision() {
+		SimpleMath math = new SimpleMath();
+		Double firstNumber = 2D;
+		Double secondNumber = 2D;
+		Double actual = math.division(firstNumber, secondNumber);
+		Double expected = 1D;
+		Assertions.assertEquals(expected, actual,
+				() -> firstNumber + " / " + secondNumber + " Did not produce " + expected);
+	}
+
+	@Test
+	void TestMean() {
+		SimpleMath math = new SimpleMath();
+		Double firstNumber = 4D;
+		Double secondNumber = 2D;
+		Double actual = math.mean(firstNumber, secondNumber);
+		Double expected = 3D;
+		Assertions.assertEquals(expected, actual,
+				() -> firstNumber + " mean " + secondNumber + " /2 Did not produce " + expected);
+
+	}
+
+	@Test
+	void TestSquareRoot() {
+		SimpleMath math = new SimpleMath();
+		Double number = 4D;
+		Double actual = math.squareRoot(number);
+		Double expected = 2D;
+		Assertions.assertEquals(expected, actual, 
+				() -> number + " squareroot did not produce " + expected);
+		
 	}
 
 }
